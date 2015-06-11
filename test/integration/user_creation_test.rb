e5e29116dfec440f7b097eddad72869f7c23d92f
Rick Bacci <rickbacci@gmail.com>
2015-06-11 17:12:03 -0600

    add create user integration test

--- test/integration/user_creation_test.rb
+++ test/integration/user_creation_test.rb
@@ -1,3 +1,16 @@
-class UserCreationTest < ActionDispatch::IntegrationTest
+require 'test_helper'
 
+class UserCreationTest < ActionDispatch::IntegrationTest
+  
+
+  test "a user can be created" do
+    visit new_user_path
+
+    fill_in "Username", with: "user"
+    fill_in "Password", with: "pass"
+
+    click_button "Create Account"
+
+    assert page.has_content?("Welcome, Ricky")
+  end
 end
