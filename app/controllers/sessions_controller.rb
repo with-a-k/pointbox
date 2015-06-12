2fad3662f4f76f35dbc1f6e7ed8f1ac6a16fda35
Erik Butcher <with_a_k@mac.com>
2015-06-11 19:05:13 -0600

    Make sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.find_by(name: params[:session][:name])
+    if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+      redirect_to @user
+    else
+      flash.now[:errors] = "Invalid login"
+      render :new
+    end
+  end
+
+  def destroy
+    session.clear
+    redirect_to login_path
+  end
+end
