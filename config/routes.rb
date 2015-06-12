05b9593e2e34537c5809e4ab62618e78ac7a4a45
Rick Bacci <rickbacci@gmail.com>
2015-06-11 21:46:40 -0600

    add login path

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
    root 'welcome#index'
   
    resources :users, only: [:new, :create, :show]
-    
+  get '/login', to: 'sessions#new'
+   
 
 
   # Example of regular route:
