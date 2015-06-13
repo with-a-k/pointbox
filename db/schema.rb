0d1b3e7446240e05fe0111449f14a10b7ffe3edc
Rick Bacci <rickbacci@gmail.com>
2015-06-13 10:34:44 -0600

    add role column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150611203018) do
+ActiveRecord::Schema.define(version: 20150613163053) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.text "name"
-    t.text "password_digest"
+    t.text     "name"
+    t.text     "password_digest"
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.integer  "role",            default: 0
   end
 
 end
