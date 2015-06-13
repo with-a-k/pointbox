bd70352ce47b473245dc162740df1c0215b79bca
Rick Bacci <rickbacci@gmail.com>
2015-06-13 15:51:29 -0600

    add admin_dashboard integration test

--- test/integration/admin_dashboard_test.rb
+++ test/integration/admin_dashboard_test.rb
@@ -4,15 +4,20 @@ class AdminDashboardTest < ActionDispatch::IntegrationTest
 
   test "admin can see admin dashboard" do 
 
-    admin = User.create(name: "admin", password: 'adminpass')
+    admin = User.create(name: "admin", password: 'adminpass', role: 1)
+    ApplicationController.any_instance.stubs(:current_user).returns(admin)
 
-    visit login_path
-
-    fill_in "Name", with: "admin"
-    fill_in "Password", with: "adminpass"
-
-    click_button "Login"
+    visit admin_dashboard_index_path
 
     assert page.has_content?("Admin Dashboard"), 'Admin login unsuccessful'
+  end
+
+  test "regular user cannnot visit admin dashboard" do
+
+    user = User.create(name: 'user', password: 'pass', role: 0)
+
+    visit admin_dashboard_index_path
+
+    refute page.has_content?("Admin Dashboard"), 'User can see admin page'
   end
 end
