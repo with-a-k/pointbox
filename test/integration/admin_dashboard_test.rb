073b7e4498db59a961229c5bb4a93496e0532883
Rick Bacci <rickbacci@gmail.com>
2015-06-14 11:29:49 -0600

    create admin rewards integration test

--- test/integration/admin_dashboard_test.rb
+++ test/integration/admin_dashboard_test.rb
@@ -23,4 +23,32 @@ class AdminDashboardTest < ActionDispatch::IntegrationTest
     assert page.has_content?("The page you were looking for doesn't exist."),
                              "404 page not shown"
   end
+
+
+
+# as an admin
+# when i visit the admin panel
+# and click on 'add reward'
+# and enter the reward name
+# and enter the reward description
+# and click 'enter'
+# it should add the reward to the database
+
+
+
+  test "admin can add a reward" do
+    admin = User.create(name: "admin", password: 'adminpass', role: 1)
+    ApplicationController.any_instance.stubs(:current_user).returns(admin)
+     byebug
+    visit admin_dashboard_index_path
+    click_button "Add new reward"
+
+    fill_in "Name", with: "pizza"
+    fill_in "Description", with: "Yummy!"
+    
+    click_button "Create reward"
+
+    assert page.has_content?("Pizza"), "reward name empty"
+    assert page.has_content?("Yummy!"), "reward description empty"
+  end
 end
